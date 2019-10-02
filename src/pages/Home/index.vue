1145eba6284d9d0f46963273c58ca51601516682
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-10-02 17:54:48 +0100

    update

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -12,7 +12,7 @@
         <q-card-section class="q-pa-sm">
           <div class="row">
             <div class="col text-center text-grey-6">
-              <div class="amount text-h3 text-accent text-weight-bold">
+              <div class="amount-left text-h3 text-accent text-weight-bold">
                 150
               </div>
               points balance
@@ -20,10 +20,6 @@
                 spend points
               </div>
             </div>
-            <q-separator
-              vertical
-              inset
-            />
             <div
               class="col text-center text-grey-6"
             >
@@ -44,6 +40,11 @@
         great work! you've recycled 5% more than last week
       </div>
     </div>
+    <div class="col flex justify-center">
+      <div class="stats">
+        stats
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -54,7 +55,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 .home-showcase {
     width: 100vw;
 }
@@ -68,5 +69,25 @@ export default {
 
 .balance-card .actions {
     border-radius: 7px;
+}
+.amount-left {
+    position:relative;
+}
+.amount-left:after {
+    content: '';
+  height: 100%;
+  width: 3px;
+
+  position: absolute;
+  right: 0;
+  top: 5px;
+
+  background-color: $grey-5;
+}
+.stats {
+    width: 100%;
+    border: 3px solid $grey-6;
+    border-radius: 10px;
+    margin: 0px 10px;
 }
 </style>
