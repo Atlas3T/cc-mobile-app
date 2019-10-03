bdc8e125c090a6e53214695951f8b027c392d478
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-10-03 16:49:04 +0100

    create separate popup component

---
+++ src/components/PopupCard/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="reward-card">
+    <q-dialog
+      v-model="info.open"
+      maximised
+      content-class="reward-dialog"
+    >
+      <div class="reward-list q-pa-md">
+        <q-card class="reward-card">
+          <!-- <q-btn
+              v-close-popup
+              icon="close"
+              flat
+              round
+              dense
+              class="text-white absolute"
+              style="right:2px; top:5px;"
+            /> -->
+          <img
+            src="../../assets/mocks/rewards/pizza.jpg"
+            class="image"
+          >
+          <q-card-section style="position: relative">
+            <div class="reward-logo absolute-center">
+              <q-avatar
+                color="red"
+                text-color="white"
+                size="80px"
+              >
+                <div class="text-body1 text-center">
+                  logo
+                </div>
+              </q-avatar>
+            </div>
+            <div class="q-mt-lg q-pt-sm text-h5 text-accent text-center text-weight-bold">
+              {{ info.title }}
+            </div>
+            <div class="text-body1 text-accent text-center text-weight-bold">
+              {{ info.description }}
+            </div>
+            <div class="text-body1 text-white text-center text-weight-bold">
+              VIEW WEBSITE
+            </div>
+            <div class="text-h5 text-white text-center text-weight-bold">
+              {{ info.points }} points
+            </div>
+          </q-card-section>
+        </q-card>
+        <div class="reward-button">
+          <div
+            class=" reward-button
+              bg-secondary text-center text-white text-weight-bold q-pa-md q-mt-sm"
+          >
+            claim
+          </div>
+        </div>
+      </div>
+    </q-dialog>
+  </div>
+</template>
+<script>
+export default {
+
+  props: {
+    info: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss">
+
+</style>
--- src/pages/Rewards/index.vue
+++ src/pages/Rewards/index.vue
@@ -5,7 +5,7 @@
         <div class="col">
           <q-card
             class="reward-card"
-            @click="card=true"
+            @click="popup.open=true"
           >
             <img src="../../assets/mocks/rewards/pizza.jpg">
             <q-card-section style="position: relative">
@@ -107,72 +107,25 @@
         </div>
       </div>
     </div>
-    <div class="reward-card">
-      <q-dialog
-        v-model="card"
-        maximised
-        content-class="reward-dialog"
-      >
-        <div class="reward-list q-pa-md">
-          <q-card class="reward-card">
-            <!-- <q-btn
-              v-close-popup
-              icon="close"
-              flat
-              round
-              dense
-              class="text-white absolute"
-              style="right:2px; top:5px;"
-            /> -->
-            <img
-              src="../../assets/mocks/rewards/pizza.jpg"
-              class="image"
-            >
-            <q-card-section style="position: relative">
-              <div class="reward-logo absolute-center">
-                <q-avatar
-                  color="red"
-                  text-color="white"
-                  size="80px"
-                >
-                  <div class="text-body1 text-center">
-                    logo
-                  </div>
-                </q-avatar>
-              </div>
-              <div class="q-mt-lg q-pt-sm text-h5 text-accent text-center text-weight-bold">
-                free doughballs
-              </div>
-              <div class="text-body1 text-accent text-center text-weight-bold">
-                when you buy any large pizza at Mama Mia
-              </div>
-              <div class="text-body1 text-white text-center text-weight-bold">
-                VIEW WEBSITE
-              </div>
-              <div class="text-h5 text-white text-center text-weight-bold">
-                250 points
-              </div>
-            </q-card-section>
-          </q-card>
-          <div class="reward-button">
-            <div
-              class=" reward-button
-              bg-secondary text-center text-white text-weight-bold q-pa-md q-mt-sm"
-            >
-              claim
-            </div>
-          </div>
-        </div>
-      </q-dialog>
-    </div>
+    <PopupCard :info="popup" />
   </q-page>
 </template>
 <script>
-export default {
+import PopupCard from '../../components/PopupCard';
 
+export default {
+  components: {
+    PopupCard,
+  },
   data() {
     return {
-      card: false,
+      popup: {
+        title: 'free doughballs',
+        description: 'when you buy any large pizza at mama mia',
+        points: 250,
+        code: 'K001-KOA6-WA10-0JW1',
+        open: false,
+      },
     };
   },
 };
