08f4c9e43e0443079683f6c0b069f6314994005f
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-10-03 12:05:58 +0100

    add reward pics

--- src/pages/Rewards/index.vue
+++ src/pages/Rewards/index.vue
@@ -1,12 +1,107 @@
 <template>
-  <q-page class="column bg-white text-primary">
-    Rewards page
-    <div class="row">
-      <div class="col">
-        1
+  <q-page class="bg-white text-primary">
+    <div class="reward-list q-pa-md items-start">
+      <div class="row q-gutter-sm q-pb-sm">
+        <div class="col">
+          <q-card class="reward-card">
+            <img src="../../assets/mocks/rewards/pizza.jpg">
+            <q-card-section style="position: relative">
+              <div class="reward-logo absolute-center">
+                <q-avatar
+                  color="red"
+                  text-color="white"
+                  size="80px"
+                >
+                  <div class="text-body1 text-center">
+                    logo
+                  </div>
+                </q-avatar>
+              </div>
+              <div class="q-mt-lg q-pt-sm text-body1 text-accent text-center text-weight-bold">
+                free doughballs
+              </div>
+              <div class="text-subtitle2 text-white text-center text-weight-bold">
+                250 points
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
+
+        <div class="col">
+          <q-card class="reward-card">
+            <img src="../../assets/mocks/rewards/theme-park.jpg">
+            <q-card-section style="position: relative">
+              <div class="reward-logo absolute-center">
+                <q-avatar
+                  color="red"
+                  text-color="white"
+                  size="80px"
+                >
+                  <div class="text-body1 text-center">
+                    logo
+                  </div>
+                </q-avatar>
+              </div>
+              <div class="q-mt-lg q-pt-sm text-body1 text-accent text-center text-weight-bold">
+                two kids free
+              </div>
+              <div class="text-subtitle2 text-white text-center text-weight-bold">
+                300 points
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
       </div>
-      <div class="col">
-        2
+      <div class="row q-gutter-sm">
+        <div class="col">
+          <q-card class="reward-card">
+            <img src="../../assets/mocks/rewards/orange.jpg">
+            <q-card-section style="position: relative">
+              <div class="reward-logo absolute-center">
+                <q-avatar
+                  color="red"
+                  text-color="white"
+                  size="80px"
+                >
+                  <div class="text-body1 text-center">
+                    logo
+                  </div>
+                </q-avatar>
+              </div>
+              <div class="q-mt-lg q-pt-sm text-body1 text-accent text-center text-weight-bold">
+                free doughballs
+              </div>
+              <div class="text-subtitle2 text-white text-center text-weight-bold">
+                250 points
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
+
+        <div class="col">
+          <q-card class="reward-card">
+            <img src="../../assets/mocks/rewards/clothes.jpg">
+            <q-card-section style="position: relative">
+              <div class="reward-logo absolute-center">
+                <q-avatar
+                  color="red"
+                  text-color="white"
+                  size="80px"
+                >
+                  <div class="text-body1 text-center">
+                    logo
+                  </div>
+                </q-avatar>
+              </div>
+              <div class="q-mt-lg q-pt-sm text-body1 text-accent text-center text-weight-bold">
+                two kids free
+              </div>
+              <div class="text-subtitle2 text-white text-center text-weight-bold">
+                300 points
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
       </div>
     </div>
   </q-page>
@@ -17,5 +112,13 @@ export default {
 };
 </script>
 <style lang="scss">
+.reward-list .q-card {
+    box-shadow: inherit;
+    background-color: $grey-5;
+    border-radius: 15px;
+}
 
+.reward-logo {
+    top: 0%
+}
 </style>
